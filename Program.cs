115d3746d165682bf3a48b358329cd53e964bb35
TomaSilva1 <tomasilva302@gmail.com>
2023-06-24 18:39:27 -0300

    primera parte

--- Program.cs
+++ Program.cs
@@ -4,24 +4,48 @@ Console.WriteLine("Hello, World!");
 
 int salir;
 int opc;
+int num1;
+num1 = 0;
 salir = 1;
 
-do
+Calculador item = new Calculador();
+
+Console.WriteLine("Ingrese un numero: ");
+item.Resultado = double.Parse(Console.ReadLine());
+
+while (salir != 0)
 {
-    Console.WriteLine("Calculadora: (1)Suma, (2)Resta, (3)Producto, (4)Cociente, (5)Limpiar");
-    opc = Console.Read();
+    Console.WriteLine("Calculadora: (1)Suma, (2)Resta, (3)Producto, (4)Cociente, (5)Limpiar :");
+    opc = int.Parse(Console.ReadLine());
     switch (opc)
     {
-        case 1:
+        case 1: 
+            Console.WriteLine("Ingrese cuanto quiere sumar: ");
+            num1 = int.Parse(Console.ReadLine());
+            item.Resultado = item.Sumar(num1);
+             
         break;
         case 2:
+            Console.WriteLine("Ingrese cuanto quiere Restar: ");
+            num1 = Console.Read();
+            item.Resultado = item.Restar(num1);
         break;
         case 3:
+            Console.WriteLine("Ingrese por cuanto lo quiere multiplicar: ");
+            num1 = Console.Read();
+            item.Resultado = item.Producto(num1);
         break;
         case 4:
+            Console.WriteLine("Ingrese por cuanto lo quiere dividir: ");
+            num1 = Console.Read();
+            item.Resultado = item.Cociente(num1);
         break;
         case 5:
         break;
     }
-} while (salir != 0);
+    Console.WriteLine("Ingrese (0) Para salir.");
+    salir = int.Parse(Console.ReadLine());
+}
+    
+ 
 
--- calculadora.cs
+++ calculadora.cs
@@ -1,6 +1,7 @@
+using System;
 namespace Calculadora
 {
-    public class calculadora
+    public class Calculador
     {
         private double dato;
 
@@ -14,29 +15,35 @@ namespace Calculadora
             }
         }
 
-        public void Sumar(double termino){
+        public double Sumar(double termino){
             dato = dato + termino;
+            return dato;
         }
 
-        public void Restar(double termino){
+        public double Restar(double termino){
             dato = dato - termino;
+            return dato;
         }
     
-        public void Producto(double termino){
+        public double Producto(double termino){
             dato = dato * termino;
+            return dato;
         }
 
-        public void Cociente(double termino){
+        public double Cociente(double termino){
             if (termino != 0)
             {
                 dato = dato/termino;
+                return dato;
             }else{
                 Console.WriteLine("ERROR: La division de un numero en 0 no esta definida.");
+                return dato;
             }
         }
         
-        public void limpiar(){
+        public double limpiar(){
             dato = 0;
+            return dato;
         }
     }
 }
